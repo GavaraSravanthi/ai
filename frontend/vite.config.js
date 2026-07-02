0a17b67f227c3b68456a239e189daf2959388c8d
Sravanthi Gavara <sravs@gmail.com>
2026-07-02 07:17:26 +0530

    Set base to ./ for deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Use './' for relative paths, which is safer for Static Sites
+  base: './', // Ensure this is ./
 })
